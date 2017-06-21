934843c0aaa8aab70c0ed9c22e6118bb1eecefee
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-06-21 18:54:36 +0200

    uploading and saving

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/users/edit', function(req, res, next) {
   res.send('respond with a resource');
@@ -48,7 +50,21 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
       errors: errors
     });
   } else{
-    console.log('No Errors');
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });
+
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    res.location('/');
+    res.redirect('/');
   }
 });
 
